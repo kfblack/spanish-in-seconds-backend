955d7826f51279de44aa53f742b62702b72b4056
kfblack <kalinblack21@gmail.com>
2024-02-07 11:31:46 -0500

    CRUD working for lessons

--- routes/lessons.js
+++ routes/lessons.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const lessonsCtrl = require('../controllers/lessons');
 
-router.post('/lessons', lessonsCtrl.create);
-router.delete('/lessons/:id', lessonsCtrl.delete);
-router.put('/lessons/:id', lessonsCtrl.update);
-router.get('/lessons/:id', lessonsCtrl.show);
+router.post('/', lessonsCtrl.create);
+router.delete('/:id', lessonsCtrl.delete);
+router.put('/:id', lessonsCtrl.update);
+router.get('/:id', lessonsCtrl.show);
 
 module.exports = router;
